433ea58ff388fcf599fe3a4a89be81313fb92a0c
GW <kowonp@gmail.com>
2022-01-02 14:51:40 +0900

    GET Request, Response

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
-const app = express();
 const PORT = 4000;
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  return res.send("Home");
+});
+
+app.get("/login", (req: Request, res: Response) => {
+  return res.send("Login");
+});
 
 app.listen(PORT, () => {
   console.log(`🚀 http://localhost:${PORT}`);
