5411b8ee3d01cac7155abc62f1447992b9c19a69
GW <kowonp@gmail.com>
2022-01-03 14:50:30 +0900

    Middlewares, Servers

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,32 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 const PORT = 4000;
 const app = express();
 
+const loggerMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  console.log("loggerMiddleware", req.method, req.url);
+  next();
+};
+
+const privateMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  console.log("privateMiddleware", req.url);
+
+  if (req.url === "/protected") {
+    return res.send("<h1>protected1</h1>");
+  }
+
+  next();
+};
+
+app.use(loggerMiddleware);
+app.use(privateMiddleware);
+
 app.get("/", (req: Request, res: Response) => {
-  return res.send("Home");
+  return res.send("<h1>home</h1>");
 });
 
-app.get("/login", (req: Request, res: Response) => {
-  return res.send("Login");
+app.get("/protected", (req: Request, res: Response) => {
+  return res.send("<h1>proteced2</h1>");
 });
 
 app.listen(PORT, () => {
