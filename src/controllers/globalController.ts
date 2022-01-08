5733f1afb7082aa0b414bd7800ea388974e9a983
GW <kowonp@gmail.com>
2022-01-08 14:37:03 +0900

    Array Database

--- src/controllers/globalController.ts
+++ src/controllers/globalController.ts
@@ -1,12 +1,37 @@
 import { Request, Response } from "express";
 
-const fakeUser = {
-  username: "gw",
-  isLoggedIn: true,
-};
+const sampleVideo = [
+  {
+    id: 1,
+    title: "video1",
+    content: "This is video1",
+    rating: 10,
+    comments: 5,
+    views: 20,
+    createdAt: "10 mins ago",
+  },
+  {
+    id: 2,
+    title: "video2",
+    content: "This is video2",
+    rating: 20,
+    comments: 2,
+    views: 10,
+    createdAt: "15 mins ago",
+  },
+  {
+    id: 3,
+    title: "video3",
+    content: "This is video3",
+    rating: 30,
+    comments: 3,
+    views: 15,
+    createdAt: "22 mins ago",
+  },
+];
 
 export const handleHome = (req: Request, res: Response) => {
-  return res.render("globals/home", { pageTitle: "홈", fakeUser });
+  return res.render("globals/home", { pageTitle: "홈", videos: sampleVideo });
 };
 
 export const handleJoin = (req: Request, res: Response) => {
--- src/controllers/videoController.ts
+++ src/controllers/videoController.ts
@@ -1,9 +1,52 @@
 import { Request, Response } from "express";
 
-export const handleSeeVideo = (req: Request, res: Response) => {
-  console.log("req.params", req.params);
+const sampleVideo = [
+  {
+    id: 1,
+    title: "video1",
+    content: "This is video1",
+    rating: 10,
+    comments: 5,
+    views: 20,
+    createdAt: "10 mins ago",
+  },
+  {
+    id: 2,
+    title: "video2",
+    content: "This is video2",
+    rating: 20,
+    comments: 2,
+    views: 10,
+    createdAt: "15 mins ago",
+  },
+  {
+    id: 3,
+    title: "video3",
+    content: "This is video3",
+    rating: 30,
+    comments: 3,
+    views: 15,
+    createdAt: "22 mins ago",
+  },
+];
 
-  return res.send("handleSeeVideo");
+interface IVideo {
+  id: number;
+  title: string;
+  content: string;
+  rating: number;
+  comments: number;
+  views: number;
+  createdAt: string;
+}
+
+export const handleSeeVideo = (req: Request, res: Response) => {
+  const {
+    params: { id },
+  } = req;
+  const foundVideo: IVideo | undefined = sampleVideo.find((video) => video.id === +id);
+
+  return res.render("videos/seeVideo", { pageTitle: `${foundVideo?.title}`, video: foundVideo });
 };
 
 export const handleEditVideo = (req: Request, res: Response) => {
