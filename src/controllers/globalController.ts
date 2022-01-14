1b0e5569e1d920a4b2b3be9a4996bf5f42983a30
GW <kowonp@gmail.com>
2022-01-14 15:14:20 +0900

    Search Video

--- src/controllers/globalController.ts
+++ src/controllers/globalController.ts
@@ -3,7 +3,7 @@ import Video from "../models/Video";
 
 export const handleHome = async (req: Request, res: Response) => {
   try {
-    const foundVideo = await (await Video.find({})).reverse();
+    const foundVideo = await Video.find({}).sort({ createdAt: "desc" });
     return res.render("globals/home", { pageTitle: "홈", videos: foundVideo });
   } catch (error) {
     throw new Error("handleHome error");
@@ -22,6 +22,29 @@ export const handleLogout = (req: Request, res: Response) => {
   return res.send("handleLogout");
 };
 
-export const handleSearch = (req: Request, res: Response) => {
-  return res.send("handleSearch");
+export const handleSearch = async (req: Request, res: Response) => {
+  try {
+    const {
+      query: { title },
+    } = req;
+
+    if (title === undefined) {
+      return res.render("globals/search", { pageTitle: "비디오 검색", videos: [] });
+    }
+
+    const foundVideo = await Video.find({
+      title: {
+        $regex: new RegExp(title as string, "i"), // $regex: /title/i
+      },
+    });
+
+    if (foundVideo.length === 0) {
+      return res.render("globals/search", { pageTitle: "비디오 검색", videos: [], errorMessage: "비디오를 찾을 수 없습니다." });
+    }
+
+    return res.render("globals/search", { pageTitle: "비디오 검색", videos: foundVideo });
+  } catch (error) {
+    console.log("handleSearch error");
+    return res.render("globals/search", { pageTitle: "비디오 검색", videos: [] });
+  }
 };
