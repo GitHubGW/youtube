74dcd0a0ed5b32b136c7443eb708cd4beab8cf52
GW <kowonp@gmail.com>
2022-01-15 16:13:11 +0900

    Create Account

--- src/controllers/globalController.ts
+++ src/controllers/globalController.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from "express";
 import Video from "../models/Video";
+import User from "../models/User";
 
 export const handleHome = async (req: Request, res: Response): Promise<void> => {
   try {
     const foundVideo = await Video.find({}).sort({ createdAt: "desc" });
     return res.render("globals/home", { pageTitle: "홈", videos: foundVideo });
   } catch (error) {
-    throw new Error("handleHome error");
+    console.log("handleHome error");
+    return res.render("globals/home", { pageTitle: "홈", videos: [] });
   }
 };
 
@@ -14,14 +16,28 @@ export const handleGetJoin = (req: Request, res: Response): void => {
   return res.render("globals/join", { pageTitle: "회원가입" });
 };
 
-export const handlePostJoin = (req: Request, res: Response) => {
+export const handlePostJoin = async (req: Request, res: Response): Promise<void> => {
   try {
-    const { body } = req;
-    console.log("aa", body);
+    const {
+      body: { username, email, password, confirmPassword },
+    } = req;
+
+    const existingUsernameOrEmail: boolean = await User.exists({ $or: [{ username }, { email }] });
+
+    if (existingUsernameOrEmail) {
+      return res.status(400).render("globals/join", { pageTitle: "회원가입", errorMessage: "이미 존재하는 유저명 또는 이메일입니다." });
+    }
+
+    if (password !== confirmPassword) {
+      return res.status(400).render("globals/join", { pageTitle: "회원가입", errorMessage: "비밀번호가 일치하지 않습니다." });
+    }
+
+    await User.create({ username, email, password });
+    return res.redirect("/login");
   } catch (error) {
     console.log("handlePostJoin error");
+    return res.status(400).render("globals/join", { pageTitle: "회원가입", errorMessage: "회원가입에 실패하였습니다." });
   }
-  return res.render("globals/join", { pageTitle: "회원가입" });
 };
 
 export const handleLogin = (req: Request, res: Response) => {
@@ -55,6 +71,6 @@ export const handleSearch = async (req: Request, res: Response): Promise<void> =
     return res.render("globals/search", { pageTitle: "비디오 검색", videos: foundVideo });
   } catch (error) {
     console.log("handleSearch error");
-    return res.render("globals/search", { pageTitle: "비디오 검색", videos: [] });
+    return res.status(400).render("globals/search", { pageTitle: "비디오 검색", videos: [] });
   }
 };
