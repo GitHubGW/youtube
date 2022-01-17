c4c4ab18870a2b15505ddf26a28d9fb9931099f0
GW <kowonp@gmail.com>
2022-01-17 16:53:29 +0900

    Connect MongoStore

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,21 @@ import "./db";
 import express, { Express } from "express";
 import morgan from "morgan";
 import session from "express-session";
+import { localsMiddleware } from "./middlewares";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import MongoStore from "connect-mongo";
 
 const PORT: number = 4000;
 const app: Express = express();
 
-app.use(morgan("dev"));
-app.set("views", process.cwd() + "/src/views");
 app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
-app.use(session({ secret: "hello", resave: true, saveUninitialized: true }));
+app.use(session({ secret: "hello", resave: false, saveUninitialized: false, store: MongoStore.create({ mongoUrl: "mongodb://localhost:27017/youtube-portfolio" }) }));
+app.use(localsMiddleware);
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
