dd76047871196b0078ac93585dcb0240e99204af
GW <kowonp@gmail.com>
2022-01-17 16:52:31 +0900

    Created middleware

---
+++ src/middlewares.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from "express";
+
+export const localsMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  console.log("localsMiddleware", req.session.loggedInUser, req.session.isLoggedIn);
+
+  if (req.session.isLoggedIn === true) {
+    res.locals.loggedInUser = req.session.loggedInUser;
+    res.locals.isLoggedIn = req.session.isLoggedIn;
+  }
+
+  next();
+};
