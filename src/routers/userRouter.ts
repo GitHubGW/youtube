bdd78622a5f6a5f96a6cbdcd33382beecfe2ada9
GW <kowonp@gmail.com>
2022-01-24 01:10:49 +0900

    Avatar Upload

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -8,13 +8,13 @@ import {
   handlePostChangeProfile,
   handleSeeUser,
 } from "../controllers/userController";
-import { multerMiddleware, privateMiddleware } from "../middlewares";
+import { avatarMulterMiddleware, privateMiddleware } from "../middlewares";
 
 const userRouter: Router = express.Router();
 
 userRouter.get("/profile", handleProfile);
 userRouter.get("/profile/edit", privateMiddleware, handleGetEditProfile);
-userRouter.post("/profile/edit", privateMiddleware, multerMiddleware.single("avatar"), handlePostEditProfile);
+userRouter.post("/profile/edit", privateMiddleware, avatarMulterMiddleware.single("avatar"), handlePostEditProfile);
 userRouter.get("/password/change", privateMiddleware, handleGetChangeProfile);
 userRouter.post("/password/change", privateMiddleware, handlePostChangeProfile);
 userRouter.get("/delete", privateMiddleware, handleDeleteUser);
