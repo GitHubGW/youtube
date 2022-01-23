cd56bae6d27faa8f51983372c5add632bc7dd07f
GW <kowonp@gmail.com>
2022-01-24 00:22:35 +0900

    Set express.static

--- src/server.ts
+++ src/server.ts
@@ -26,6 +26,7 @@ app.use(
   })
 );
 app.use(localsMiddleware);
+app.use("/uploads", express.static("uploads"));
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
