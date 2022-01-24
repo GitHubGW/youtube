d040075474f0460e2790b1e15af9582847856051
GW <kowonp@gmail.com>
2022-01-24 15:18:29 +0900

    User Profile

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,12 +2,22 @@ import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import User, { UserInterface } from "../models/User";
 
-export const handleProfile = (req: Request, res: Response): void => {
-  return res.render("users/profile", { pageTitle: `${req.session.loggedInUser?.username} 프로필` });
+export const handleSeeUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const foundUser: UserInterface | null = await User.findOne({ username: req.params.username });
+
+    if (foundUser === null) {
+      throw new Error();
+    }
+    return res.render("users/seeUser", { pageTitle: `${req.params.username} 프로필`, user: foundUser });
+  } catch (error) {
+    console.log("handleSeeUser error");
+    return res.status(404).render("404", { pageTitle: "페이지를 찾을 수 없습니다." });
+  }
 };
 
 export const handleGetEditProfile = (req: Request, res: Response): void => {
-  return res.render("users/editProfile", { pageTitle: "프로필 수정" });
+  return res.render("users/editProfile", { pageTitle: `${req.params.username} 프로필 수정` });
 };
 
 export const handlePostEditProfile = async (req: Request, res: Response): Promise<void> => {
@@ -27,7 +37,7 @@ export const handlePostEditProfile = async (req: Request, res: Response): Promis
     return res.redirect("/users/profile/edit");
   } catch (error) {
     console.log("handlePostEditProfile error");
-    return res.status(400).render("users/editProfile", { pageTitle: "프로필 수정", errorMessage: "이미 존재하는 이름 또는 이메일입니다." });
+    return res.status(400).render("users/editProfile", { pageTitle: `${req.params.username} 프로필 수정`, errorMessage: "이미 존재하는 이름 또는 이메일입니다." });
   }
 };
 
@@ -37,7 +47,7 @@ export const handleGetChangeProfile = (req: Request, res: Response): void => {
   } = req;
 
   if (loggedInUser?.githubId === null && loggedInUser.kakaoId === null) {
-    return res.render("users/changePassword", { pageTitle: "비밀번호 변경" });
+    return res.render("users/changePassword", { pageTitle: `${req.params.username} 비밀번호 변경` });
   }
   return res.redirect("/users/profile");
 };
@@ -57,10 +67,12 @@ export const handlePostChangeProfile = async (req: Request, res: Response): Prom
     const isMatchedPassword: boolean = await bcrypt.compare(oldPassword, foundUser?.password as string);
 
     if (isMatchedPassword === false) {
-      return res.status(400).render("users/changePassword", { pageTitle: "비밀번호 변경", errorMessage: "기존 비밀번호가 일치하지 않습니다." });
+      return res.status(400).render("users/changePassword", { pageTitle: `${req.params.username} 비밀번호 변경`, errorMessage: "기존 비밀번호가 일치하지 않습니다." });
     }
     if (newPassword !== newPasswordConfirm) {
-      return res.status(400).render("users/changePassword", { pageTitle: "비밀번호 변경", errorMessage: "새 비밀번호와 새 비밀번호 확인이 일치하지 않습니다." });
+      return res
+        .status(400)
+        .render("users/changePassword", { pageTitle: `${req.params.username} 비밀번호 변경`, errorMessage: "새 비밀번호와 새 비밀번호 확인이 일치하지 않습니다." });
     }
 
     const updatedUser = await User.findByIdAndUpdate(loggedInUser?._id, { $set: { password: newPassword } }, { new: true });
@@ -68,14 +80,10 @@ export const handlePostChangeProfile = async (req: Request, res: Response): Prom
     return res.redirect("/users/profile");
   } catch (error) {
     console.log("handlePostChangeProfile error");
-    return res.status(400).render("users/changePassword", { pageTitle: "비밀번호 변경", errorMessage: "비밀번호 변경에 실패하였습니다." });
+    return res.status(400).render("users/changePassword", { pageTitle: `${req.params.username} 비밀번호 변경`, errorMessage: "비밀번호 변경에 실패하였습니다." });
   }
 };
 
 export const handleDeleteUser = (req: Request, res: Response) => {
   return res.send("handleDeleteUser");
-};
-
-export const handleSeeUser = (req: Request, res: Response) => {
-  return res.send("handleSeeUser");
 };
--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,23 +1,14 @@
 import express, { Router } from "express";
-import {
-  handleDeleteUser,
-  handleProfile,
-  handleGetEditProfile,
-  handlePostEditProfile,
-  handleGetChangeProfile,
-  handlePostChangeProfile,
-  handleSeeUser,
-} from "../controllers/userController";
+import { handleDeleteUser, handleSeeUser, handleGetEditProfile, handlePostEditProfile, handleGetChangeProfile, handlePostChangeProfile } from "../controllers/userController";
 import { avatarMulterMiddleware, privateMiddleware } from "../middlewares";
 
 const userRouter: Router = express.Router();
 
-userRouter.get("/profile", handleProfile);
-userRouter.get("/profile/edit", privateMiddleware, handleGetEditProfile);
-userRouter.post("/profile/edit", privateMiddleware, avatarMulterMiddleware.single("avatar"), handlePostEditProfile);
-userRouter.get("/password/change", privateMiddleware, handleGetChangeProfile);
-userRouter.post("/password/change", privateMiddleware, handlePostChangeProfile);
+userRouter.get("/:username", handleSeeUser);
+userRouter.get("/:username/profile/edit", privateMiddleware, handleGetEditProfile);
+userRouter.post("/:username/profile/edit", privateMiddleware, avatarMulterMiddleware.single("avatar"), handlePostEditProfile);
+userRouter.get("/:username/password/change", privateMiddleware, handleGetChangeProfile);
+userRouter.post("/:username/password/change", privateMiddleware, handlePostChangeProfile);
 userRouter.get("/delete", privateMiddleware, handleDeleteUser);
-userRouter.get("/:id", handleSeeUser);
 
 export default userRouter;
