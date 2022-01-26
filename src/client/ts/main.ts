837995df55f9019749a7648c28637a446a09ab0e
GW <kowonp@gmail.com>
2022-01-26 23:11:35 +0900

    Finished Webpack Configuration

--- src/client/ts/main.ts
+++ src/client/ts/main.ts
@@ -1,9 +1,7 @@
+import "../scss/style.scss";
+
 const handleHello = async (): Promise<void> => {
   const count: number = 10;
-  console.log("hello");
-  await fetch("");
 };
 
 handleHello();
-
-alert("hi");
